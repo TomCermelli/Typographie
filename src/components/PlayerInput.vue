7aabea0e2ae58fed58d0e770fe1b713b8e048dfc
amullertak <abrume@DESKTOP-D75S2L0.localdomain>
2020-01-09 21:40:08 +0100

    Jeu terminé bye

---
+++ src/components/PlayerInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input id="playerinput" type="text" v-model="playerinput" v-on:keyup="checkWord" />
+    <h3>{{score}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayerInput",
+  data() {
+    return {
+      playerinput: "",
+      score: 0
+    };
+  },
+  props: {
+    word: String
+  },
+  methods: {
+    checkWord: function() {
+      if (this.playerinput == this.word) {
+        this.$emit("updateWord");
+        this.playerinput = "";
+        this.score += 1;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Words.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="words">
+    <h3>{{word}}</h3>
+    <PlayerInput v-bind:word="word" @updateWord="updateWord" />
+  </div>
+</template>
+
+<script>
+import PlayerInput from "./PlayerInput";
+export default {
+  name: "Words",
+  data() {
+    return {
+      word: this.findWords()
+    };
+  },
+  components: {
+    PlayerInput
+  },
+  methods: {
+    updateWord: function() {
+      this.word = this.findWords();
+    },
+    findWords: function() {
+      const words = [
+        "about",
+        "abandon",
+        "absolutely",
+        "laboratory",
+        "leader",
+        "letter",
+        "library",
+        "location",
+        "narrow",
+        "newspaper",
+        "nowhere",
+        "separated",
+        "service",
+        "session",
+        "shelter",
+        "table",
+        "target",
+        "thirsty",
+        "tomorrow",
+        "tourist",
+        "element",
+        "embarassed",
+        "enthusiastically",
+        "eventually",
+        "everywhere",
+        "happily",
+        "hesitate",
+        "homework",
+        "oddly",
+        "otherwise",
+        "packaging",
+        "peaceful",
+        "possibility",
+        "qualification",
+        "reduction",
+        "relationship",
+        "routine",
+        "ultimate",
+        "unexpectedly",
+        "vocabulary",
+        "whisper"
+      ];
+      const b = words.length;
+      const word = words[Math.floor(Math.random() * b)];
+      return word;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+  color: #42b983;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
